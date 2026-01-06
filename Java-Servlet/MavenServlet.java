d164dbf060be56f8e45da735b7dd48cc4b56b45d
Manitej <151040895+Manitej-04@users.noreply.github.com>
2026-01-06 10:00:48 +0530

    Update MavenServlet.java

--- Java-Servlet/MavenServlet.java
+++ Java-Servlet/MavenServlet.java
@@ -1,23 +1,24 @@
-//package com.api.testing;
-//
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//@WebServlet("/MavenServlet")
-//public class MavenServlet extends HttpServlet {
-//
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-//	}
-//
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		response.getWriter().print("<h2>Hello....</h2>");
-//	}
-//
-//}
+package com.api.testing;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/MavenServlet")
+public class MavenServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().print("<h2>Hello....</h2>");
+	}
+
+}
+
